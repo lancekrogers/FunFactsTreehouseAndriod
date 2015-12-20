7f42e0fc47010c93a14504dcac9688648e0fa14d
Lance Rogers <lancekincaid1994@gmail.com>
2015-12-20 15:58:56 -0500

    learning the framework

--- FunFacts/app/src/main/java/com/lancekrogers/funfacts/FunFactsActivity.java
+++ FunFacts/app/src/main/java/com/lancekrogers/funfacts/FunFactsActivity.java
@@ -1,5 +1,6 @@
 package com.lancekrogers.funfacts;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class FunFactsActivity extends AppCompatActivity {
 
@@ -18,14 +21,18 @@ public class FunFactsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
+
+        // Declare View Variables and assign views to id
+
+        TextView factLabel = (TextView) findViewById(R.id.fact);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        View.OnClickListener() listener = new View.OnClickListener(){
+            @overide
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
             }
-        });
+        };
+        showFactButton.setOnClickListener();
     }
 
     @Override
