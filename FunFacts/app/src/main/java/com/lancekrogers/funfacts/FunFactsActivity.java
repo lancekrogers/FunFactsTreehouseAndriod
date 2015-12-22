fd9f1543bbd3a6474396eeac609896bfdc6d2768
Lance Rogers <lancekincaid1994@gmail.com>
2015-12-22 10:35:15 -0500

    Added random color background before starting the lesson

--- FunFacts/app/src/main/java/com/lancekrogers/funfacts/FunFactsActivity.java
+++ FunFacts/app/src/main/java/com/lancekrogers/funfacts/FunFactsActivity.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
 
     private FactBook mFactBook = new FactBook();
+    private Random randomNum = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +24,14 @@ public class FunFactsActivity extends AppCompatActivity {
 
         final TextView factLabel = (TextView) findViewById(R.id.fact);
         Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                relativeLayout.setBackgroundColor(randomNum.nextInt() + 1);
                 String fact = mFactBook.getFact();
                 factLabel.setText(fact);
+
             }
         };
         showFactButton.setOnClickListener(listener);
