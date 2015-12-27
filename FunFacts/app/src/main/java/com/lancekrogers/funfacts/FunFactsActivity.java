67daa8db9522b90c578eb18f136700deffdb4a31
Lance Rogers <lancekincaid1994@gmail.com>
2015-12-27 07:26:55 -0500

    tag added to log debug messages

--- FunFacts/app/src/main/java/com/lancekrogers/funfacts/FunFactsActivity.java
+++ FunFacts/app/src/main/java/com/lancekrogers/funfacts/FunFactsActivity.java
@@ -2,6 +2,7 @@ package com.lancekrogers.funfacts;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
@@ -11,6 +12,8 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class FunFactsActivity extends AppCompatActivity {
+
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
 
     private FactBook mFactBook = new FactBook();
     private Random randomNum = new Random();
@@ -40,8 +43,10 @@ public class FunFactsActivity extends AppCompatActivity {
             }
         };
         showFactButton.setOnClickListener(listener);
-        String message = "Oohra our activity was created!";
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        //String message = "Oohra our activity was created!";
+        //Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+        Log.d(TAG,"We're logging from the onCreate() method");
+        
     }
 }
 
